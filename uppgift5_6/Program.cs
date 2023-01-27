4620d4dd47e86dbfd2bb0951afa5269cad5ec8d3
Caspian2 <casa04001@utb.vaxjo.se>
2023-01-27 10:26:12 +0100

    Add project files.

---
+++ uppgift5_6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Uppgift5_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = new int[5];
+            Console.WriteLine("Skriva in tal som ska multiplaceras. (5 stycken)");
+            for (int i = 0; i < 5; i++)
+            {
+                tal[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine("*");
+            }
+            int summa = tal[0] * tal[1] * tal[2] * tal[3] * tal[4];
+            Console.WriteLine("den summa är: " +summa+".");
+        }
+    }
+}
